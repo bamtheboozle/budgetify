e7cf983595c68ed892c5ece0d64d76bca2270eaa
Dragos-Paul Marinescu <dragos.marinescu@innio.com>
2020-08-05 16:48:31 +0300

    fix dep cycle

--- App.js
+++ App.js
@@ -4,15 +4,6 @@ import Navigation from "./src/navigation/Navigation";
 import AuthContext from "./src/contexts/AuthContext";
 import { fetchAuthState } from "./src/storageHelpers/auth";
 
-export const PAGES = {
-  GetStarted: "GetStarted",
-  Auth: "Auth",
-  Home: "Home",
-  Activity: "Activity",
-  Goals: "Goals",
-  Budgets: "Budgets",
-};
-
 export default function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
 
